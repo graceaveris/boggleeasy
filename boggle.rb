ec038df67c3eaf9f87e816d0ea652e2ec09d91e0
Grace Averis <39510617+graceaveris@users.noreply.github.com>
2018-06-12 15:15:22 +0800

    Add files via upload

--- boggle.rb
+++ boggle.rb
@@ -1,16 +1,38 @@
 
 #An array of random letters
 
-letters = ('a'..'z').to_a
+my_dice = [
+["Qu", "Qu", "Qu", "Qu", "Qu", "Qu"], #dice 0
+["E", "L", "R", "T", "T", "Y"], #dice 1 etc...
+["A", "A", "E", "E", "G", "N"],
+["E", "L", "R", "T", "T", "Y"],
+["A", "A", "E", "E", "G", "N"],
+["E", "L", "R", "T", "T", "Y"],
+["A", "A", "B", "E", "G", "N"],
+["S", "L", "R", "T", "N", "Y"],
+["A", "A", "E", "E", "Qu", "N"],
+["E", "L", "R", "T", "T", "Y"],
+["S", "M", "E", "E", "G", "N"],
+["E", "L", "R", "T", "T", "Y"],
+["A", "S", "E", "E", "G", "N"],
+["E", "L", "K", "T", "T", "Y"],
+["S", "A", "E", "B", "G", "N"],
+["E", "O", "R", "T", "T", "Y"]
+]
+
 board = ['-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-']
 
-length = letters.length
+length = my_dice.length
+dice_length = my_dice[0].length
 
 board.each_with_index do |thing, index|
-	board[index] = letters[rand(length)]
+	board[index] = my_dice[index][rand(dice_length)] # in this area, we need to access each dcue one after another
 end
 
 newboard = board.each_slice(4).to_a
 newboard.each do |x|
-	p x 
+	p x
 end
+
+# rand(length)
+
